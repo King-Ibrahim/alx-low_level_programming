b1418216124c2ac97fa88b1e7beace40ac31b97f
KIng-Ibrahim <mdotibrahim@gmail.com>
2023-02-24 17:36:06 -0800

    no space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,11 +18,11 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
         	if (i % 3 == 0)
-			printf(" fizz");
+			printf("fizz");
         	else if (i % 5 == 0)
-			printf(" buzz");
+			printf("buzz");
         	else if ((i % 3 == 0) && (i % 5 == 0))
-			printf(" fizzbuzz");
+			printf("fizzbuzz");
 		else
 			printf("%d ", i);
 
