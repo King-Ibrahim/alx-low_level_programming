7a11146ca03d2408d1ea120984a92f6db9662fe4
KIng-Ibrahim <mdotibrahim@gmail.com>
2023-02-24 18:04:03 -0800

    oh betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if ((i == 2) || (i == 4))
-			continue;
+		if ((i == '2') || (i == '4'))
+			;
 		else
 			_putchar(i);
 	}
