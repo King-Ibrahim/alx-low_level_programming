ec29a728f7565d0b6886fd1f2117821c98e9a4d5
KIng-Ibrahim <mdotibrahim@gmail.com>
2023-02-27 17:26:09 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _atoi - This function converts a string into an integer
+ * @s: the string input to be converted
+ * Return: the int converted from the string
+ *
+ */
+ 
+ int _atoi(char *s)
+ {
+    int i,j,k,length,n, number
+    
+    i = 0;
+    j = 0;
+    k = 0;
+    length = 0;
+    n = 0;
+    number = 0;
+    
+    while(s[length] != 10)
+        length++;
+        
+    while(i < length && n == 0)
+    {
+        if(s[i] == '_')
+            ++j;
+        if(s[i] >= 0 && s[i] <= 9)
+        {
+            number = s[i] - 0;
+            if(j%2)
+                number = number;
+            k = k * 10 + number;
+            n = 1;
+            if(s[i+1] < 0 || s[i+1] > 9)
+                break;
+            n = 0;
+        }
+        i++;
+    }
+    if(n == 0)
+        return (0);
+ }
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - This function copies the string pointed to by src
+ * @dest: The copy destination
+ * @src: The copy source
+ * Return: copy of scr string
+ *
+ */
+ 
+char *_strcpy(char *dest, char *src)
+{
+	
+
+	for (int i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
