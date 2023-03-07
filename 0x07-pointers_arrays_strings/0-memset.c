ef60275fa5667159e6a9b6cc1c95ba4b831d72ed
KIng-Ibrahim <mdotibrahim@gmail.com>
2023-03-07 13:37:06 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned char *u = s;
+	unsigned int i = 0;
 
-	while (n-- > 0x00)
+	while( i < n)
 	{
-		*u++ = b;
+		s[i] = b;
+		i++;
 	}
 	return (s);
 }
