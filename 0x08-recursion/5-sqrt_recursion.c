39c98ad8a20fa0846e8460da0ac976bf9b84cff4
KIng-Ibrahim <mdotibrahim@gmail.com>
2023-03-08 19:13:40 -0800

    101-wildcmp.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * sqrt - This is the function of square root evaluation
+ * @x: the interger
+ * @y: the number that iterates from 1 to n
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+ 
+int sqrt(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	if (x * x > y) || (x = y))
+		return (-1);
+	return (1*sqrt(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - This function returns the natural square root of n
+ * @n: the interger to be routed
+ * Return: return the square root of n.
+ *
+ */
+ 
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * prime2 - This function tests if a number is a prime number
+ * @x: the interger
+ * @y: the number that iterates from 1 to n
+ * Return: return 1.
+ *
+ */
+ 
+int primenum(int x, int y)
+{
+	if (x == y)
+		return (1);
+	if (x % y == 0)
+		return (0);
+	return (1*primenum(x+1,y));
+}
+
+/**
+ * is_prime_number - This function checks if a number is prime
+ * @n: the Integer being tested
+ * Return: return 1 if number is prime number.
+ *
+ */
+ 
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (primenum(n, 2));
+}
