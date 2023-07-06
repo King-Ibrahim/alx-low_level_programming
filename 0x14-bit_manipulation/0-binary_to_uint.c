8cf3a559420f63300e4705e582aea1ac2dea2b7d
King-Ibrahim <mdotibrahim@gmail.com>
2023-07-06 08:31:36 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,6 @@ unsigned int binary_to_uint(const char *b)
 	unint = 0;
 
 	for (length = 0; b[length] != '\0'; length++)
-		;
 
 	for (length--, base_two = 1; length >= 0; length--, base_two *= 2)
 	{
