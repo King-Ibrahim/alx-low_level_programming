513599d07ef7d77cca3ffd856c6b51ff81cf13dc
King-Ibrahim <mdotibrahim@gmail.com>
2023-07-06 08:29:50 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,27 +6,33 @@
  * @b: the  binary.
  * Return: unsigned int.
  *
- */
+**/
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int length;
+	unsigned int unint;
+	int base_two, length;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
 
-	for (length = 0; b[length];)
-		length++;
+	unint = 0;
 
-	for (length -= 1; length >= 0; length--)
+	for (length = 0; b[length] != '\0'; length++)
+		;
+
+	for (length--, base_two = 1; length >= 0; length--, base_two *= 2)
 	{
 		if (b[length] != '0' && b[length] != '1')
+		{
 			return (0);
+		}
 
-		num += (b[length] - '0') * mult;
-		mult *= 2;
+		if (b[length] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	return (num);
+	return (unint);
 }
